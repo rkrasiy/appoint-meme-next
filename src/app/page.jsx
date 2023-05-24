758eca163f8a262817564403db7045c6afee65a9
Ruslan <krasiyruslan@gmail.com>
2023-05-24 08:18:36 +0200

    Refs #1002: Impement mocks

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,16 +12,12 @@ export default function Home() {
             name: "ruslan",
             id: "1"
         }
-        const url = "https://krasiy.com/booking/"
-        console.log('Send: ', data)
-        const response = await fetch(url, {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data), // body data type must match "Content-Type" header
-        });
-        console.log(response)
+        const url = "http://krasiy.com:8080/booking"
+       // console.log('Send: ', data)
+        const response = await fetch(url)
+
+        const json = await response.json()
+        console.log( json)
     };
 
     return (
